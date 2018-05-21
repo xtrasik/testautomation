a14ed483d9f8733bc43a28671ff5145c83b5683c
xtrasik <xtrasik@gmail.com>
2018-05-21 13:47:40 +0200

    First Version of App with first created pom.xml

---
+++ qatestautomation/src/test/java/testautomation/JunitTest.java
@@ -0,0 +1,14 @@
+package testautomation;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JunitTest {
+
+    @Test
+    public void firstTest(){
+        assertTrue ( true) ;
+    }
+}
