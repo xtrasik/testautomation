48e7403709d9c16fd7049dbd9d4c3f5e0c69298d
xtrasik <xtrasik@gmail.com>
2018-05-24 14:13:32 +0200

    new module frontend

---
+++ qaapi/src/test/java/com/jsystems/apitest/ApiTest.java
@@ -0,0 +1,15 @@
+package com.jsystems.apitest;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ApiTest {
+@Test
+    @DisplayName("First REST api Test")
+    public void apiFirstTest(){
+
+}
+
+
+}
